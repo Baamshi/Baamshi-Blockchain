dcb3d375b528af3abdc71b8fa40e428536a58286
Reza Ebrahimi <baamshiproduction@gmail.com>
2022-11-07 19:46:09 +0330

    add 1

---
+++ server/src/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, flash, request, redirect, url_for, session, logging
+from passlib.hash import sha256_crypt
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get("PORT", 8080)))
